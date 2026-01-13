72917d4da460119665902234357acb34bbadc7b2
Dannika <71364766+dannikate@users.noreply.github.com>
2026-01-13 11:25:28 +1000

    updated saipan chloride JSON

--- src/utils/dataSource.js
+++ src/utils/dataSource.js
@@ -7,6 +7,6 @@ Return:
 // update this with data set; must be full path, starting from the root directory 
 // const geoJsonUrl = "./src/data/dummy_data.json";
 
-const geoJsonUrl = "./src/data/saipan_chloride2.json";
+const geoJsonUrl = "./src/data/saipan_chloride_2026.json";
 
 export { geoJsonUrl }
